ba052cf0c4b4ccb5d849b09922cf8793453c7c93
matdev93 <112671782+matdev93@users.noreply.github.com>
2022-11-19 22:25:19 -0300

    Prueba

--- main.js
+++ main.js
@@ -48,3 +48,97 @@ function validarCreateForm(e) {
         
     }
 }
+
+function agregarArticulo () {
+    listaArticulos.push ({...objArticulo});
+    mostrarArticulos ();
+    createForm.reset ();
+    limpiarObjeto();
+    
+}
+    
+function limpiarObjeto() {
+    objArticulo.id = '';
+    objArticulo.nombre = '';
+    objArticulo.descripcion = '';
+    objArticulo.precio = '';
+    objArticulo.imagen = '';
+}
+
+function mostrarArticulos() {
+    limpiarHTML ();
+    const divArticulos = document.querySelector('.div-articulos')
+
+    listaArticulos.forEach( articulo => {
+        const {id, nombre, descripcion, precio, imagen} = articulo;
+
+        const carta = document.createElement('div')
+        carta.classList.add('card-wrapper')
+        carta.innerHTML = `<div class="card-header">
+                                <img src="${imagen}"/>
+                                 <h3>${nombre}</h3>
+                           </div>
+                           <div class="card-detail">
+                            <p>${descripcion}</p>
+                            <p>$ ${precio}</p>
+                           </div>`
+        carta.dataset.id = id;
+
+        const editarBoton = document.createElement('button');
+        editarBoton.onclick = () => cargarArticulo (articulo);
+        editarBoton.textContent = 'Editar';
+        editarBoton.classList.add('btn', 'btn-success');
+        carta.append (editarBoton);
+
+        const eliminarBoton = document.createElement('button');
+        eliminarBoton.onclick = () => eliminarArticulo (id);
+        eliminarBoton.textContent = 'Eliminar';
+        eliminarBoton.classList.add('btn' , 'btn-danger');
+        carta.append(eliminarBoton);
+
+        // const hr =document.createElement('hr');
+
+        divArticulos.appendChild(carta);
+        // divArticulos.appendChild(hr)
+        
+    });
+
+    localStorage.setItem("items",JSON.stringify(listaArticulos));
+}
+
+function cargarArticulo(articulo) {
+    const {id, nombre, descripcion, precio, imagen} = articulo;
+    nombreInput.value = nombre;
+    descripcionInput.value = descripcion;
+    precioInput.value = precio;
+    imagenInput.value = imagen;
+
+    objArticulo.id = id;
+
+    createForm.querySelector('button[type="submit"]').text = 'Actualizar'
+
+    editando = true;
+}
+
+function editarArticulo () {
+    objArticulo.nombre = nombreInput.value;
+    objArticulo.descripcion= descripcionInput.value;
+    objArticulo.precio = precioInput.value;
+    objArticulo.imagen = imagenInput.value;
+    listaArticulos.map ( articulo => {
+        if(articulo.id === objArticulo.id) {
+            articulo.id = objArticulo.id;
+            articulo.nombre = objArticulo.nombre;
+            articulo.descripcion = objArticulo.descripcion;
+            articulo.precio = objArticulo.precio;
+            articulo.imagen = objArticulo.imagen;
+            
+        }
+    });
+
+    limpiarHTML();
+    mostrarArticulos();
+    createForm.reset();
+    createForm.querySelector('button[type="submit"]').textContent = 'Agregar'
+    editando = false;
+}
